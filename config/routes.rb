bef784a16f84738a3a0ddf2e6ee7c584af3c6b9d
hata <mh.mundus@gmail.com>
2020-05-29 15:39:44 +0900

    Ruby 道場2 finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   get "/" => "home#top"
   get "/about" => "home#about"
   get "/posts/index" => "posts#index"
+  get "/posts/new" => "posts#new"
+
+  post "/posts/create" => "posts#create"
+  post "/posts/:id/update" => "posts#update"
+  post "/posts/:id/destroy" => "posts#destroy"
+
   get "/posts/:id" => "posts#show"
+  get "/posts/:id/edit" => "posts#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
