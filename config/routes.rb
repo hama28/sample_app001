12b58f676ce738be7ebd7c2223784760047cad94
hata <mh.mundus@gmail.com>
2020-06-03 21:53:15 +0900

    道場手前　passwordカラムの追加ができない

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "/login" => "users#login_form"
+  post "/login" => "users#login"
+  post "/logout" => "users#logout"
+
   get "/" => "home#top"
   get "/about" => "home#about"
   get "/posts/index" => "posts#index"
@@ -10,5 +14,14 @@ Rails.application.routes.draw do
 
   get "/posts/:id" => "posts#show"
   get "/posts/:id/edit" => "posts#edit"
+
+  get "/users/index" => "users#index"
+  get "/users/:id" => "users#show"
+  get "/signup" => "users#new"
+  get "/users/:id/edit" => "users#edit"
+
+  post "/users/create" => "users#create"
+  post "/users/:id/update" => "users#update"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
