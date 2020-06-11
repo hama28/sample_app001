a2b33a689dc7c6d5b7efa6dd975f5ec1b71b943b
hata <mh.mundus@gmail.com>
2020-06-11 22:57:27 +0900

    202006112257

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_130041) do
+ActiveRecord::Schema.define(version: 2020_06_11_134856) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
